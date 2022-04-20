ca9c4f854806cebdc41e90067a38454b81f12365
DavidBen98 <davidbenedette@hotmail.com>
2022-04-20 16:08:10 -0300

    Estilos varios

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -14,9 +14,9 @@ const About = () => {
                     <h1 className="about-container__h1">Sobre mí</h1>
                     <p className="about-container__text">
                     Soy una persona entusiasta y apasionada por el desarrollo web y la tecnología. <br/>
-                    Conocí este mundo hace un poco mas de 2 años a través de un amigo, y, 
-                    desde el momento que vi los problemas que se podían solucionar por medio de código, 
-                    supe que me quería dedicar a esto. 
+                    Conocí este mundo hace un poco mas de 2 años a través de un amigo, y supe que me
+                    quería dedicar a esto desde que ví los problemas que se podían solucionar 
+                    por medio de código. <br/>
                     Por esta razón estudié Tecnicatura en Computación, en donde aprendí diferentes tecnologías 
                     como C#, SQL y JavaScript. 
                     Además, sigo formándome de manera autodidacta para obtener mi primer empleo en la industria IT.

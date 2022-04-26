49eeebd0d5a864903069df119f16bff97f74252d
DavidBen98 <davidbenedette@hotmail.com>
2022-04-26 12:06:33 -0300

    Incorporación logos en card

--- src/components/projects/CardItem.js
+++ src/components/projects/CardItem.js
@@ -11,6 +11,13 @@ const CardItem = (props) => {
                     />
                 <h2 className="card__title">{props.title}</h2>
                 <p className="card__description">{props.description}</p>
+                <div className='card__tech'>
+                    {props.tech.map((logo)=>
+                        logo === "/static/media/logo-php.7c41b19923da57634a76372016ed129e.svg"?  
+                        <img alt="logo de tecnología" src={logo} className="card__logo" style={{paddingTop:"5px"}}/>
+                        : <img alt="logo de tecnología" src={logo} className="card__logo"/>
+                    )}
+                </div>
             </div>
             <div className="card__btn">
                 <button className="card__button" target="_blank" onClick={() => window.open(props.project, "_blank")}>Visitar</button>

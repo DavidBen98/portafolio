34e46a1140d123bc64a3e28706fbc62e2b49267d
DavidBen98 <davidbenedette@hotmail.com>
2022-04-26 12:06:46 -0300

    Incorporación logos en card

--- src/components/projects/Cards.js
+++ src/components/projects/Cards.js
@@ -1,6 +1,12 @@
 import CardItem from "./CardItem";
 import ecommerce from "../../assets/ecommerce.png";
 import buscador from "../../assets/buscador.png";
+import react from "../../assets/logo-react.svg";
+import javascript from "../../assets/logo-javascript.png";
+import html from "../../assets/logo-html.svg";
+import css from "../../assets/logo-css.svg";
+import php from "../../assets/logo-php.svg";
+import Mysql from "../../assets/logo-mysql.svg";
 
 const Cards = () => {
     return (
@@ -12,6 +18,7 @@ const Cards = () => {
                 compra con mercado pago..."
                 link="https://github.com/DavidBen98/sitio_web_php_js"
                 project="https://mueblesgiannis.000webhostapp.com/"
+                tech={[javascript,html,css,php,Mysql]}
             />
 
             <CardItem
@@ -20,6 +27,7 @@ const Cards = () => {
                 description="Web 3 es el futuro y por eso este proyecto soluciona muchos problemas..."
                 link="https://github.com/DavidBen98/sitio_web_php_js"
                 project="https://buscador-canciones.surge.sh/"
+                tech={[javascript,html,css,php,Mysql]}
             />
 
             <CardItem
@@ -28,6 +36,7 @@ const Cards = () => {
                 description="Busca canciones con su respectivo artista, y guarda en favoritos las deseadas."
                 link="https://github.com/DavidBen98/crud-canciones"
                 project="https://buscador-canciones.surge.sh/"
+                tech={[react,html,css]}
             />
         </section>
     );
